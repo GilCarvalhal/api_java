58e8e787b15f87613536a7bb869e3cf22d4a5329
GilCarvalhal <gilbertocarvalhalmatos@gmail.com>
2025-06-12 11:31:01 -0300

    Consumindo api

--- src/main/java/org/aplicacao/Main.java
+++ src/main/java/org/aplicacao/Main.java
@@ -1,9 +1,23 @@
 package org.aplicacao;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import org.aplicacao.dto.EnderecoDto;
+import org.aplicacao.servico.ApiServico;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá mundo!");
+
+        ApiServico apiServico = new ApiServico();
+
+        try {
+            EnderecoDto enderecoDto = apiServico.getEndereco("01001000");
+            System.out.println(enderecoDto.getLogradouro());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
+}
